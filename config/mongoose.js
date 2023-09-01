814671ef20857511972886958fab415ce722f553
Amitkumar0821 <107250715+Amitkumar0821@users.noreply.github.com>
2023-09-01 12:42:17 +0530

    Update mongoose.js
    change mongoose url

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,13 @@
 // setting up the mongodb connection through mongoose
 
 const mongoose = require('mongoose');
-const url = 'mongodb+srv://amitkumarmandal126:amit1234@cluster0.llwj3d2.mongodb.net/Issue_Tracker?retryWrites=true&w=majority';
+mongoose.connect('mongodb+srv://amitkumarmandal126:WhgUYzcdva3NNHk9@issue.i3kkouk.mongodb.net/?retryWrites=true&w=majority');
 
 
 // mongoose.connect('url') 
-mongoose.connect(url,{
-    serverSelectionTimeoutMS: 10000,
-});
+// mongoose.connect(url,{
+//     serverSelectionTimeoutMS: 10000,
+// });
 const db=mongoose.connection;
 
 //  catch an error when connecting mongodb
